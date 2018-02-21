43ff5363239afe88d174b5551a00d8f57b2a6089
Douglas Mendes <mendes-barreto@live.com>
2018-02-21 10:03:41 -0300

    Start to remove the main storyboard from project

--- DitMoi/AppDelegate.swift
+++ DitMoi/AppDelegate.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UI
         return true
     }
 
